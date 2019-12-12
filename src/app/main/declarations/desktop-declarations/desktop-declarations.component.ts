41f517182ee4205b53adc7398f2320a27afcbebe
Tom Goossens <tomrai@live.nl>
2019-12-12 17:47:00 +0100

    declarations naar MVC gerefactored

---
+++ src/app/main/declarations/desktop-declarations/desktop-declarations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-declarations',
+  templateUrl: './desktop-declarations.component.html',
+  styleUrls: ['./desktop-declarations.component.scss']
+})
+export class DesktopDeclarationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/main/declarations/mobile-declarations/mobile-declarations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-declarations',
+  templateUrl: './mobile-declarations.component.html',
+  styleUrls: ['./mobile-declarations.component.scss']
+})
+export class MobileDeclarationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
