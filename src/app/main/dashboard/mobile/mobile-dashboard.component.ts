8cf4b2e90a288168899714f84cb5001b4713de69
Tom Goossens <tomrai@live.nl>
2019-12-12 14:48:53 +0100

    dashboard separate desktop and mobile fixed

---
+++ src/app/main/dashboard/mobile/mobile-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-dashboard',
+  templateUrl: './mobile-dashboard.component.html',
+  styleUrls: ['./mobile-dashboard.component.scss']
+})
+export class MobileDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
