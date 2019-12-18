19db6a8a1e4098d3374e7f9e7fd77fd5e6195afa
Ole Timmers <s1115222@studen.hsleiden.nl>
2019-12-18 16:03:16 +0100

    tabel profiel-projecten mooi gemaakt

--- src/app/main/profile/profile-projects/profile-projects.component.ts
+++ src/app/main/profile/profile-projects/profile-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Project} from './project.model';
 
 @Component({
   selector: 'app-profile-projects',
@@ -6,10 +7,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-projects.component.scss']
 })
 export class ProfileProjectsComponent implements OnInit {
+  private maxCountPage = 6;
+  public projects: Project[];
+  public selectedProjects: Project[];
+  public pageNumberMinimum = 0;
+  public pageNumberMaximum = this.maxCountPage;
 
-  constructor() { }
+  private generateEmptyRows: number;
+  public emptyRowsList;
+
+  public allCheckboxesSelected = false;
+
+  public pageBtnLeft = true;
+  public pageBtnRight = true;
+
+  constructor() {
+    // hier worden alle projecten in geladen
+    this.projects = [
+      new Project("","Project 1", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 2", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 3", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 4", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 5", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 6", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 7", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 8", "", "17-12-2019", "18-12-2019"),
+      new Project("","Project 9", "", "17-12-2019", "18-12-2019")
+    ];
+    this.checkEmptyRows();
+    this.checkButtons();
+  }
 
   ngOnInit() {
+  }
+
+
+
+
+  // Wisselen van pagina's
+  getMinimum() {
+    return this.pageNumberMinimum;
+  }
+
+  getMaximum() {
+    return this.pageNumberMaximum;
+  }
+
+  nextPage() {
+    if (!(this.pageNumberMinimum + this.maxCountPage > this.projects.length)) {
+      this.pageNumberMinimum += this.maxCountPage;
+      this.pageNumberMaximum += this.maxCountPage;
+      this.checkEmptyRows();
+      this.checkButtons()
+    }
+  }
+
+  prevPage() {
+    if (this.pageNumberMinimum > 0) {
+      this.pageNumberMinimum -= this.maxCountPage;
+      this.pageNumberMaximum -= this.maxCountPage;
+      this.checkEmptyRows()
+      this.checkButtons()
+    }
+  }
+
+  private checkEmptyRows() {
+    this.generateEmptyRows = this.pageNumberMaximum - this.projects.length;
+    if (this.generateEmptyRows < 1){
+      this.generateEmptyRows = 0;
+    }
+    this.emptyRowsList = Array(this.generateEmptyRows).fill(1);
+  }
+
+  private checkButtons(){
+    if(this.pageNumberMinimum < 2){
+      this.pageBtnLeft = false;
+    } else {this.pageBtnLeft= true;}
+    if(this.pageNumberMinimum + this.maxCountPage > this.projects.length){
+      this.pageBtnRight = false;
+    } else {this.pageBtnRight = true;}
   }
 
 }
