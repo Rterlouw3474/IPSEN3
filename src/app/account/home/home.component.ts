eaa7e8d003ea4b6d425e3f5045cabe2287c59ae0
Bahlok <richard-terlouw@hotmail.com>
2020-01-29 19:24:33 +0100

    homepage done

--- src/app/account/home/home.component.ts
+++ src/app/account/home/home.component.ts
@@ -9,8 +9,22 @@ import {LoadService} from '../../services/load.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private load: LoadService) { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  isMobile() {
+    if (window.innerWidth < 768) {
+      return true;
+    } else { return false; }
+  }
+
+  routerUrlEquals(route) {
+    if (this.isMobile()) {
+      const hash = window.location.hash;
+      const pattern = new RegExp(`${route}`);
+      return pattern.test(hash);
+    } else { return true; }
   }
 }
